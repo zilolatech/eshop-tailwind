4fddd463dde7b340a943b79785fa3619df20ae3e
zi1o1a <zilolanader@gmail.com>
2025-01-10 18:29:30 +0500

    fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,18 +6,18 @@ const Form = () => {
         <form className='border-black border-[1px] rounded p-5 w-full'>
             <h2 className='text-2xl'>Delivery Form</h2>
             <h3 className='text-xl my-3'>Full Name</h3>
-            <div id="full-name">
+            <div id="full-name" className='py-2'>
                 <input className='w-full md:w-[46%] border-black border-[1px] mr-5 rounded p-2' type="text" id="fname" name="fname" placeholder="First Name" required />
                 <input className='w-full md:w-[46%] border-black border-[1px]  mt-5 md:mt-0 rounded p-2' type="text" id="lname" name="lname" placeholder="Last Name" required />
             </div>
             <h3 className='text-xl my-3'>Address</h3>
-            <div>
+            <div className='py-2'>
                 <input className='w-full md:w-[46%] border-black border-[1px] mr-5 rounded p-2' type="text" id="s-address" name="s-address" placeholder="Street Address" required />
                 <input className='w-full md:w-[46%] border-black border-[1px] mt-5 md:mt-0 rounded p-2' type="text" id="city" name="city" placeholder="City" required />
                 <input className='w-full md:w-[46%] border-black border-[1px] mt-5 rounded p-2' type="text" id="zip-code" name="zip-code" placeholder="Zip Code" required />
             </div>
             <h3 className='text-xl my-3'>Phone Number</h3>
-            <div>
+            <div className='py-2'>
                 <input className='w-full md:w-[46%] border-black border-[1px] rounded p-2' type="tel" id="tel" name="tel" placeholder="Phone Number" required />
             </div>
             <h3 className='text-xl my-3'>E-mail</h3>
